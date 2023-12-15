b7470f8e3b112c13e39134f30e5319248c815093
conanoc <conanoc@gmail.com>
2023-12-15 14:42:02 +0900

    fix package resolution
    Signed-off-by: conanoc <conanoc@gmail.com>

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 import class Foundation.ProcessInfo
 
-let package = Package(
+var package = Package(
     name: "aries-uniffi-wrappers",
     platforms: [
         .macOS(.v10_15),
@@ -33,9 +33,6 @@ let package = Package(
             name: "anoncreds_uniffiFFI",
             url: "https://github.com/hyperledger/aries-uniffi-wrappers/releases/download/0.1.1-binary/anoncreds_uniffiFFI.xcframework.zip",
             checksum: "41f8d517d89f57ca28598d936b8743c8e378ef929a58ac00bdbf77dffe1e19b7"),
-        .binaryTarget(
-            name: "anoncreds_uniffiFFI_local",
-            path: "anoncreds/out/anoncreds_uniffiFFI.xcframework"),
         .target(
             name: "Askar",
             path: "swift/Sources/Askar"),
@@ -47,9 +44,6 @@ let package = Package(
             name: "askar_uniffiFFI",
             url: "https://github.com/hyperledger/aries-uniffi-wrappers/releases/download/0.1.1-binary/askar_uniffiFFI.xcframework.zip",
             checksum: "761220bc486d14c371c84c30481c7b07b4e8dc53a0e61cdd3e67efa842bc73ab"),
-        .binaryTarget(
-            name: "askar_uniffiFFI_local",
-            path: "askar/out/askar_uniffiFFI.xcframework"),
         .target(
             name: "IndyVdr",
             path: "swift/Sources/IndyVdr"),
@@ -63,10 +57,7 @@ let package = Package(
         .binaryTarget(
             name: "indy_vdr_uniffiFFI",
             url: "https://github.com/hyperledger/aries-uniffi-wrappers/releases/download/0.1.1-binary/indy_vdr_uniffiFFI.xcframework.zip",
-            checksum: "076821ffbeb291e2541dbe8eb6186e2583cbadd68cde144c999cf0362fe9a19c"),
-        .binaryTarget(
-            name: "indy_vdr_uniffiFFI_local",
-            path: "indy-vdr/out/indy_vdr_uniffiFFI.xcframework"),
+            checksum: "076821ffbeb291e2541dbe8eb6186e2583cbadd68cde144c999cf0362fe9a19c")
     ]
 )
 
@@ -79,6 +70,16 @@ if ProcessInfo.processInfo.environment["USE_LOCAL_XCFRAMEWORK"] == nil {
     askarTarget?.dependencies.append("askar_uniffiFFI")
     indyVdrTarget?.dependencies.append("indy_vdr_uniffiFFI")
 } else {
+    package.targets.append(.binaryTarget(
+        name: "anoncreds_uniffiFFI_local",
+        path: "anoncreds/out/anoncreds_uniffiFFI.xcframework"))
+    package.targets.append(.binaryTarget(
+        name: "askar_uniffiFFI_local",
+        path: "askar/out/askar_uniffiFFI.xcframework"))
+    package.targets.append(.binaryTarget(
+        name: "indy_vdr_uniffiFFI_local",
+        path: "indy-vdr/out/indy_vdr_uniffiFFI.xcframework"))
+
     anoncredsTarget?.dependencies.append("anoncreds_uniffiFFI_local")
     askarTarget?.dependencies.append("askar_uniffiFFI_local")
     indyVdrTarget?.dependencies.append("indy_vdr_uniffiFFI_local")
